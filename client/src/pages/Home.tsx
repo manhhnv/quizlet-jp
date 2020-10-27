7104c54f1f8f6f23d50a9d8e9a3dccf3b98ac922
Minh Trần Quang <minhtq30102000@gmail.com>
2020-10-28 01:39:24 +0700

    HeroSection

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../components/layouts/Header';
+import HeroSection from '../components/layouts/HeroSection';
 import Footer from '../components/layouts/Footer';
 const HomePage = () => {
     return (
         <React.Fragment>
             <Header />
-            
+            <HeroSection />
             <Footer />
         </React.Fragment>
     )

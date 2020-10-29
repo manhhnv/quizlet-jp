4cfa6cf2421598af520e1a09fd948cb379608f9e
minhglhf <minhtq30102000@gmail.com>
2020-10-29 22:36:53 +0700

    login api serve

---
+++ server/src/migrations/1603985591703-migration-database.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class migrationDatabase1603985591703 implements MigrationInterface {
+    name = 'migrationDatabase1603985591703'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("CREATE TABLE `users` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `email` varchar(255) NOT NULL, `password` varchar(255) NOT NULL, `birthday` varchar(255) NOT NULL, `updatedAt` varchar(255) NOT NULL, `createdAt` varchar(255) NOT NULL, `role` varchar(255) NOT NULL, UNIQUE INDEX `IDX_97672ac88f789774dd47f7c8be` (`email`), PRIMARY KEY (`id`)) ENGINE=InnoDB");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("DROP INDEX `IDX_97672ac88f789774dd47f7c8be` ON `users`");
+        await queryRunner.query("DROP TABLE `users`");
+    }
+
+}
